c3ec3a8c2568434719471af773d9cbc9556074e9
aa <szerr@sina.cn>
2017-10-30 16:50:30 +0800

    增加不同使用方式的实例代码

--- example_proxy_test.go
+++ example_proxy_test.go
@@ -7,10 +7,30 @@ import (
 	"time"
 )
 
-//抓取和验证免费http代理，提供远程或本地客户端
+//服务示例
+func Example_server() {
+	go proxy.BuildProxy()
+	go proxy.Server(":8082")
+	llend := 0
+	log.Println("Start...")
+	for {
+		lend := 0
+		time.Sleep(time.Second)
+		proxy.ProxyMap.Range(func(key interface{}, value interface{}) bool {
+			lend += 1
+			return true
+		})
+		if lend != llend {
+			log.Println("lend:", lend)
+			llend = lend
+		}
+	}
+
+}
+
+//使用远程代理
 func Example_client() {
-	next := proxy.ProxyClient("", 3) //使用本地的代理数据
-	//next := proxy.ProxyClient("http://127.0.0.1:8082", 3) //推荐从服务器获取代理数据
+	next := proxy.ProxyClient("http://127.0.0.1:8082", 3) //推荐从服务器获取代理数据
 	client := &http.Client{
 		Timeout: time.Second * 3,
 	}
@@ -21,5 +41,28 @@ func Example_client() {
 			log.Fatal(err)
 		}
 		client.Get("http://www.icanhazip.com/")
+	}
+}
+
+//用本地服务获取ip
+func Example_local() {
+	go proxy.BuildProxy() //抓取/验证服务
+	next := proxy.ProxyClient("", 6)
+	client := &http.Client{Timeout: time.Second * 6} //可以在这里定义除代理外的其他client属性 }
+	URL := "http://www.baidu.com"
+	for err := next(client); err == nil; err = next(client) {
+		resp, err := client.Get(URL)
+		if err != nil {
+			log.Println(err)
+			continue
+		} else {
+			data, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			log.Println(len(data))
+		}
+		time.Sleep(time.Second * 1)
 	}
 }
